2acf60778995c91160bd9a3e6b79c5569078def2
Geiler Orlando Hipia Mejia <geilerhipiamejia0@hotmail.com>
2026-09-24 11:26:22 -0500

    Publicar MarIAna (8e5d7ac)

---
+++ assets/RutaJugar-019ZZZkV.js
@@ -0,0 +1,1 @@
+import{l as e}from"./dist-CxYe84mW.js";import{yt as t}from"./index-CMTRPANr.js";import{PaginaJugar as n}from"./PaginaJugar-CEdmsSIe.js";var r=e(),i=t(`/jugar`);function a(){let{nivel:e,jugadores:t}=i.useSearch();return(0,r.jsx)(n,{},`${e??`todo`}-${t??1}`)}export{a as RutaJugar};
